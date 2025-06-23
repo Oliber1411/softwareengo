0ff52961451344da541769e798c93109520bf5d5
Oliber1411 <Oliver.vanraalte@education.nsw.gov.au>
2025-06-23 10:08:08 +1000

    Update ChallengeC.py

--- Lesson like 6 I forgot to do this/ChallengeC.py
+++ Lesson like 6 I forgot to do this/ChallengeC.py
@@ -1,4 +1,11 @@
-baddie = ["insult", "ignored", "apologised", "praised"]
 
-badthing = input("What did they do? " + str(baddie))
-likes = input("did they like your recent posts? (yes, no (case sensitive))")
+badthing = input("What did they do? (ignored, insult, apologised, praised) ")
+likes = (input("did they like your recent posts? (True / False)(case sensitive))"))
+friend = (input("Are they your friend in real life? (True / False)"))
+
+if (badthing == "insult" or "ignored") and (friend == "False"):
+    print("BLOCKED!") 
+elif (badthing == "insult" or "ignored") and (friend == "True"):
+    print("Mute")
+else:
+    print("Follow")
