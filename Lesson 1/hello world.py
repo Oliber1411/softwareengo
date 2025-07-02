6d4508227935134fc20a840a4452aa66e659b79b
Oliber1411 <Oliver.vanraalte@education.nsw.gov.au>
2025-07-02 14:42:13 +1000

    I am so cool so updates ppl updates

--- Lesson 1/hello world.py
+++ Lesson 1/hello world.py
@@ -1,1 +1,3 @@
 print("Hello world!")
+name = "Oliver"
+print("Hello " + name)
--- Lesson like 6 I forgot to do this/ChallengeD.py
+++ Lesson like 6 I forgot to do this/ChallengeD.py
@@ -1,12 +1,41 @@
 weapons = ["Sword", "Axe", "Bow"]
 special_attacks = ["Flame burst", "Ice shards", "Shade strike", "Earth palm"]
-choosenweapon = input("Choose weapon:" + str(weapons))
+enemies = []
+enemies.append("Bow goblin")
+enemies.append("Sword goblin")
+enemies.append("Fire elementals")
+bow_distance = 10
+bow_hp = 3
+sword_distance = 3
+sword_hp = 5
+element_distance = 5
+element_hp = 6
+bow_damage = 1
+swordnaxe_damage = 2
+special_damage = 2
+while element_hp > 0 or sword_hp > 0 or bow_hp > 0:
+    
+    attacking = input("Choose an enemy to attack" + str(enemies))
+    if attacking in enemies:
+        choosenweapon = input("Choose weapon to attack with:" + str(weapons))
 
-if choosenweapon in weapons: 
-    choosenspecial = input("Choose special attack" + str(special_attacks))
-    if choosenspecial in special_attacks:
-        print("Your weapon is " + str(choosenweapon) + " and your special attack is " + str(choosenspecial))
+        if choosenweapon in weapons: 
+            choosenspecial = input("Choose special attack" + str(special_attacks))
+            if choosenspecial in special_attacks:
+                print("You attack " + (attacking) + " using a " + (choosenweapon) + " and your special attack " + (choosenspecial))
+                if attacking == "Bow goblin" and choosenweapon == "Bow":
+                    print("You attack the bow goblin dealing " + str(bow_damage + special_damage) + " damage" )
+                    bow_hp == bow_hp - (bow_damage + special_damage)
+                    if bow_hp >= 0:
+                        enemies.remove("Bow goblin")
+                else:
+                    print("Your " + choosenweapon + " is too short and cant get the bow goblin, however your " + choosenspecial + "can and it does" + str(special_damage))
+                    bow_hp == bow_hp - special_damage        
+            else:
+                print("You tried to cast a spell you didnt know, not the best idea, now your dead!")
+        else:
+            print("You didnt choose a weapon and you died, womp womp")
     else:
-        print("You tried to cast a spell you didnt know, not the best idea, now your dead!")
-else:
-    print("You didnt choose a weapon and you died, womp womp")
+        print("You didnt attack an enemy and died, sucks to be you")
+
+
