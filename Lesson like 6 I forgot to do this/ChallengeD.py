95e479fc9a2c3f562d18f2b5d0f082c500d74c36
Oliber1411 <Oliver.vanraalte@education.nsw.gov.au>
2025-07-03 13:27:36 +1000

    w

--- Lesson like 6 I forgot to do this/ChallengeD.py
+++ Lesson like 6 I forgot to do this/ChallengeD.py
@@ -1,41 +1,26 @@
+player_hp = 10
+sword_goblin_hp = 5
+bow_goblin_hp = 3
+turn = 0
 weapons = ["Sword", "Axe", "Bow"]
 special_attacks = ["Flame burst", "Ice shards", "Shade strike", "Earth palm"]
-enemies = []
-enemies.append("Bow goblin")
-enemies.append("Sword goblin")
-enemies.append("Fire elementals")
-bow_distance = 10
-bow_hp = 3
-sword_distance = 3
-sword_hp = 5
-element_distance = 5
-element_hp = 6
-bow_damage = 1
-swordnaxe_damage = 2
-special_damage = 2
-while element_hp > 0 or sword_hp > 0 or bow_hp > 0:
-    
-    attacking = input("Choose an enemy to attack" + str(enemies))
-    if attacking in enemies:
-        choosenweapon = input("Choose weapon to attack with:" + str(weapons))
 
-        if choosenweapon in weapons: 
-            choosenspecial = input("Choose special attack" + str(special_attacks))
-            if choosenspecial in special_attacks:
-                print("You attack " + (attacking) + " using a " + (choosenweapon) + " and your special attack " + (choosenspecial))
-                if attacking == "Bow goblin" and choosenweapon == "Bow":
-                    print("You attack the bow goblin dealing " + str(bow_damage + special_damage) + " damage" )
-                    bow_hp == bow_hp - (bow_damage + special_damage)
-                    if bow_hp >= 0:
-                        enemies.remove("Bow goblin")
-                else:
-                    print("Your " + choosenweapon + " is too short and cant get the bow goblin, however your " + choosenspecial + "can and it does" + str(special_damage))
-                    bow_hp == bow_hp - special_damage        
-            else:
-                print("You tried to cast a spell you didnt know, not the best idea, now your dead!")
-        else:
-            print("You didnt choose a weapon and you died, womp womp")
+chosen_weapon = input("What weapon would you like to equip?" + str(weapons))
+if chosen_weapon in weapons:
+    print("You equiped " + chosen_weapon)
+    chosen_spc = input("What is your special attack?" + str(special_attacks))
+    if chosen_spc in special_attacks:
+        print("You gain " + chosen_spc + "Now you're ready to battle")
+        turn = 1
     else:
-        print("You didnt attack an enemy and died, sucks to be you")
+        print("You tried to cast a spell you dont know, it backfired, you're dead now :)")
+else:
+    print("You didnt have a weapon to defend yourself and you died, womp womp")
 
-
+if turn == 1:
+    attacking = input("Two enemies stand before you, would you like to attack sword goblin or bow goblin?")
+    if attacking == "bow goblin" and chosen_weapon == "Bow":
+        print("You fire at the bow goblin with your bow and hit it with your " + chosen_spc + " which finishes it off")
+        bow_goblin_hp = 0
+    elif:
+    attacking == "bow goblin" and chosen_weapon != "Bow"
